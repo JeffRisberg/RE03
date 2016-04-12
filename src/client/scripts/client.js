068c2b10dffc32926f6ed92653c1a4552abd2d83
JeffRisberg <jrisberg@aol.com>
2016-04-12 00:12:48 -0700

    converted server over to structure similar to RE04, updated Babel version

--- src/client/scripts/client.js
+++ src/client/scripts/client.js
@@ -3,39 +3,43 @@ import App from '../../app';
 
 var attachElement = document.getElementById('app');
 
-var state = {
-  cart: {
-    title: 'Jurassic Park Animals',
-    items: [
-      {
+var basicItems = [
+    {
         title: 'Stegosaurus',
         description: "Mild",
         price: 42
-      },
-      {
+    },
+    {
         title: 'Velociraptor',
         description: "Mean",
         price: 71
-      },
-      {
-        title: 'Tyrannosaur',
-        description: "Really Mean",
-        price: 133
-      },
-      {
+    },
+    {
         title: 'Lame-brained Lawyer',
         description: "Stupid",
         price: 10
-      }
-    ]
-  },
-  channel: {
-    title: "Disney Channel",
-    shows: [
-      { name: "The Lion King", channelId: "LK-564" },
-      { name: "Pocahontas", channelId: "P-789" }
-    ]
-  }
+    }
+];
+
+var state = {
+    cart: {
+        title: 'Jurassic Park Animals',
+        items: [
+            ...basicItems,
+            {
+                title: 'Lame-brained Lawyer',
+                description: "Stupid",
+                price: 10
+            }
+        ]
+    },
+    channel: {
+        title: "Disney Channel",
+        shows: [
+            {name: "The Lion King", channelId: "LK-564"},
+            {name: "Pocahontas", channelId: "P-789"}
+        ]
+    }
 };
 
 var app;
@@ -44,7 +48,7 @@ Debug.enable('myApp*');
 
 // Create new app and attach to element
 app = new App({
-  state: state
+    state: state
 });
 
 app.renderToDOM(attachElement);
--- src/server/index.js
+++ src/server/index.js
@@ -1,3 +1,20 @@
-require('babel/register');
+var path = require('path');
+var globSync = require('glob').sync;
+var express = require('express');
+var app = express();
 
-module.exports = require('./server');
+const PATH_STYLES = path.resolve(__dirname, '../client/styles');
+const PATH_DIST = path.resolve(__dirname, '../../dist');
+
+app.use('/styles', express.static(PATH_STYLES));
+app.use(express.static(PATH_DIST));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/index.html'));
+});
+
+var server = app.listen(process.env.PORT || 3000, () => {
+    var port = server.address().port;
+
+    console.log('Server is listening at %s', port);
+});
