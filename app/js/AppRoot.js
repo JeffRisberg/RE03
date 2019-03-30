f79be5eecd3b4ba85897452150ba7d5e526c196a
JeffRisberg <jrisberg@aol.com>
2019-03-30 15:54:31 -0700

    updated to React Router 5

--- app/js/AppRoot.js
+++ app/js/AppRoot.js
@@ -1,36 +1,46 @@
-import React, { Component } from 'react';
-import { Route, NavLink } from 'react-router-dom';
+import React, {Component} from 'react';
+import {PropTypes} from 'prop-types';
+import {NavLink, Route, Router, Switch} from 'react-router-dom';
 import Home from './pages/Home';
 import Items from './pages/Items';
 import Events from './pages/Events';
 
 class AppRoot extends Component {
+  static propTypes = {
+    history: PropTypes.object.isRequired
+  };
 
   render() {
-    return (
-      <div>
-        <div className="navbar navbar-inverse navbar-fixed-top" style={{ marginBottom: '0px' }}>
-          <div className="container">
-            <div className="navbar-inner">
-              <NavLink to="/" className="navbar-brand">RE03</NavLink>
+    const {history} = this.props;
 
-              <div className="navbar-collapse collapse">
-                <ul className="nav navbar-nav">
-                  <li><NavLink to="/items">Items</NavLink></li>
-                  <li><NavLink to="/events">Events</NavLink></li>
-                </ul>
+    return (
+      <Router history={history}>
+        <div>
+          <div className="navbar navbar-inverse navbar-fixed-top" style={{marginBottom: '0px'}}>
+            <div className="container">
+              <div className="navbar-inner">
+                <NavLink to="/" className="navbar-brand">RE03</NavLink>
+
+                <div className="navbar-collapse collapse">
+                  <ul className="nav navbar-nav">
+                    <li><NavLink to="/items">Items</NavLink></li>
+                    <li><NavLink to="/events">Events</NavLink></li>
+                  </ul>
+                </div>
               </div>
             </div>
           </div>
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Home}/>
+              <Route exact path="/items" component={Items}/>
+              <Route exact path="/items/detail/:id" component={Items}/>
+              <Route exact path="/events" component={Events}/>
+              <Route exact path="/events/detail/:id?" component={Events}/>
+            </Switch>
+          </div>
         </div>
-        <div className="container">
-          <Route exact path="/" component={Home}/>
-          <Route exact path="/items" component={Items}/>
-          <Route exact path="/items/detail/:id" component={Items}/>
-          <Route exact path="/events" component={Events}/>
-          <Route exact path="/events/detail/:id?" component={Events}/>
-        </div>
-      </div>
+      </Router>
     );
   }
 }
--- app/js/client.js
+++ app/js/client.js
@@ -1,21 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { Route } from 'react-router-dom';
-import { ConnectedRouter } from 'react-router-redux';
+import {Provider} from 'react-redux';
 import createHistory from 'history/createBrowserHistory';
 import AppRoot from './AppRoot';
 import configureStore from './configureStore';
 
-const history = createHistory({ basename: '/' });
+const history = createHistory({basename: '/'});
 
-const store = configureStore({ initialState: {}, history });
+const store = configureStore({initialState: {}, history});
 
 ReactDOM.render(
   <Provider store={store}>
-    <ConnectedRouter history={history}>
-      <Route path="/" component={AppRoot}/>
-    </ConnectedRouter>
+    <AppRoot history={history}/>
   </Provider>,
   document.getElementById('app-root')
 );
